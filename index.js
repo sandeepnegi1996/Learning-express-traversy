5dd8722e66327be3219a40d8a51a09a3bc79db0b
sandeepnegi1996 <9041128179sandy@gmail.com>
2019-08-28 13:47:00 +0530

    basic setup done

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send(
+    "hello world leaning express!!! this is changed and testing nodemon"
+  );
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log("server is running");
+});
