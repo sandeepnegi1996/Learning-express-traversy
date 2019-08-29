28f397623f045906c8539f05e99e30cfcec45cfd
sandeepnegi1996 <9041128179sandy@gmail.com>
2019-08-29 11:48:43 +0530

    added the basic authentication

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const logger = require("./middleware/Logger");
+const basicAuth = require("./_helpers/basic-auth");
+const errorHandler = require("./_helpers/error-handler");
+
 //adding the middleware for the parsing
-app.use(express.json());
 
 //for form submition
 
 app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 //creating a middleware
 //which is inside the middleware folder and exported
@@ -18,11 +21,16 @@ app.use(express.urlencoded({ extended: false }));
 //app.use(express.static(path.join(__dirname, "public")));
 
 //this route get all members
+app.use(basicAuth);
 
 app.use("/api/members", require("./routes/api/members"));
+
+app.use("/users", require("./routes/users/users.controller"));
+
+app.use(errorHandler);
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-  console.log("server is running");
+  console.log(`server is running on port ${port}`);
 });
